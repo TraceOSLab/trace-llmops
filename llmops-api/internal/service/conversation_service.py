e37d9661d0b25d9930bf65723fc484bd89ac91f4
youyou <s.qiu@foxmail.com>
2026-08-24 13:32:59 +0800

    feat: 更新接口文档

--- llmops-api/internal/service/conversation_service.py
+++ llmops-api/internal/service/conversation_service.py
@@ -5,6 +5,7 @@
 @Time   :   2026/1/21 10:52
 @Author :   s.qiu@foxmail.com
 """
+
 import logging
 from dataclasses import dataclass
 from typing import Any
@@ -17,8 +18,14 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 
 from internal.core.agent.entities.queue_entity import AgentThought, QueueEvent
-from internal.entity.conversation_entity import SUMMARIZER_TEMPLATE, CONVERSATION_NAME_TEMPLATE, ConversationInfo, \
-    SUGGESTED_QUESTIONS_TEMPLATE, SuggestedQuestions, InvokeFrom
+from internal.entity.conversation_entity import (
+    SUMMARIZER_TEMPLATE,
+    CONVERSATION_NAME_TEMPLATE,
+    ConversationInfo,
+    SUGGESTED_QUESTIONS_TEMPLATE,
+    SuggestedQuestions,
+    InvokeFrom,
+)
 from internal.model import Conversation, Message, MessageAgentThought
 from pkg.sqlalchemy import SQLAlchemy
 from .base_service import BaseService
@@ -28,18 +35,23 @@ from .base_service import BaseService
 @dataclass
 class ConversationService(BaseService):
     """会话服务"""
+
     db: SQLAlchemy
 
-    def summary(self, human_message: str, ai_message: str, old_summary: str = "") -> str:
+    def summary(
+        self, human_message: str, ai_message: str, old_summary: str = ""
+    ) -> str:
         """根据消息和旧的摘要生成 新摘要"""
         prompt = ChatPromptTemplate.from_template(SUMMARIZER_TEMPLATE)
         llm = ChatOpenAI(model="glm-4.7", temperature=0.5)
         # 构建链应用
         chain = prompt | llm | StrOutputParser()
-        new_summary = chain.invoke({
-            "new_lines": f"Human: {human_message}\nAI: {ai_message}",
-            "summary": old_summary,
-        })
+        new_summary = chain.invoke(
+            {
+                "new_lines": f"Human: {human_message}\nAI: {ai_message}",
+                "summary": old_summary,
+            }
+        )
 
         return new_summary
 
@@ -47,10 +59,9 @@ class ConversationService(BaseService):
         """根据 query 生成当前 会话名称"""
 
         # 提示词 注意：语言与用户的输入保持一致
-        prompt = ChatPromptTemplate.from_messages([
-            ("system", CONVERSATION_NAME_TEMPLATE),
-            ("human", "{query}")
-        ])
+        prompt = ChatPromptTemplate.from_messages(
+            [("system", CONVERSATION_NAME_TEMPLATE), ("human", "{query}")]
+        )
         llm = ChatOpenAI(model="glm-4.7", temperature=0)
         structured_llm = llm.with_structured_output(ConversationInfo)
         chain = prompt | structured_llm
@@ -67,7 +78,9 @@ class ConversationService(BaseService):
             if conversation_info and hasattr(conversation_info, "subject"):
                 name = conversation_info.subject
         except Exception as e:
-            logging.exception(f"提取会话名称错误，conversation_info:{conversation_info},错误信息：{str(e)}")
+            logging.exception(
+                f"提取会话名称错误，conversation_info:{conversation_info},错误信息：{str(e)}"
+            )
 
         if len(name) > 50:
             name = name[:50] + "..."
@@ -77,10 +90,9 @@ class ConversationService(BaseService):
     def generate_suggested_questions(self, histories: str) -> list[str]:
         """根据历史信息生成 建议问题（不超过3条）"""
 
-        prompt = ChatPromptTemplate.from_messages([
-            ("system", SUGGESTED_QUESTIONS_TEMPLATE),
-            ("human", "{histories}")
-        ])
+        prompt = ChatPromptTemplate.from_messages(
+            [("system", SUGGESTED_QUESTIONS_TEMPLATE), ("human", "{histories}")]
+        )
         llm = ChatOpenAI(model="glm-4.7", temperature=0)
         structured_llm = llm.with_structured_output(SuggestedQuestions)
         chain = prompt | structured_llm
@@ -92,7 +104,9 @@ class ConversationService(BaseService):
             if suggested_questions and hasattr(suggested_questions, "questions"):
                 questions = suggested_questions.questions
         except Exception as e:
-            logging.exception(f"生成建议问题错误，suggested_questions:{suggested_questions}, 错误信息:{str(e)}")
+            logging.exception(
+                f"生成建议问题错误，suggested_questions:{suggested_questions}, 错误信息:{str(e)}"
+            )
 
         if len(questions) > 3:
             questions = questions[:3]
@@ -100,13 +114,15 @@ class ConversationService(BaseService):
         return questions
 
     def save_agent_thoughts(
-            self, flask_app: Flask,
-            account_id: UUID,
-            app_id: UUID,
-            app_config: dict[str, Any],
-            conversation_id: UUID,
-            message_id: UUID,
-            agent_thoughts: list[AgentThought]):
+        self,
+        flask_app: Flask,
+        account_id: UUID,
+        app_id: UUID,
+        app_config: dict[str, Any],
+        conversation_id: UUID,
+        message_id: UUID,
+        agent_thoughts: list[AgentThought],
+    ):
         """存储智能体 推理消息"""
         with flask_app.app_context():
             position = 0
@@ -149,24 +165,36 @@ class ConversationService(BaseService):
                 # 时间是否为 agent_message
                 if agent_thought.event == QueueEvent.AGENT_MESSAGE:
                     # 更新消息
-                    self.update(message, message=agent_thought.message, answer=agent_thought.answer,
-                                latency=latency)
+                    self.update(
+                        message,
+                        message=agent_thought.message,
+                        answer=agent_thought.answer,
+                        latency=latency,
+                    )
 
                     # 更新长期记忆
                     if app_config["long_term_memory"]["enable"]:
-                        new_summary = self.summary(message.query, agent_thought.answer, conversation.summary)
+                        new_summary = self.summary(
+                            message.query, agent_thought.answer, conversation.summary
+                        )
                         self.update(conversation, summary=new_summary)
 
                     # 生成会话名称
                     if conversation.is_new:
-                        new_conversation_name = self.generate_conversation_name(message.query)
+                        new_conversation_name = self.generate_conversation_name(
+                            message.query
+                        )
                         self.update(conversation, name=new_conversation_name)
 
                     # 判断是否为停止或者错误，如果是则需要更新消息状态
-                    if agent_thought.event in [QueueEvent.STOP, QueueEvent.ERROR, QueueEvent.TIMEOUT]:
+                    if agent_thought.event in [
+                        QueueEvent.STOP,
+                        QueueEvent.ERROR,
+                        QueueEvent.TIMEOUT,
+                    ]:
                         self.update(
                             message,
                             status=agent_thought.event,
-                            observation=agent_thought.observation
+                            observation=agent_thought.observation,
                         )
                         break
